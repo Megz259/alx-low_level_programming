fff61f6c5a4911c54508deda66c712a5757f60d7
Megz259 <megan.hodsonrsa@gmail.com>
2023-03-17 03:37:01 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Prints the alphabet
  *
@@ -6,13 +9,13 @@
  */
 int main(void)
 {
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+	char low;
 
-	for (i=0; i<52; i++)
-	{
-		putchar(alp[i]);
-	}
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+	
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
 	putchar('/n');
-	return (o);
+	return (0);
 }
