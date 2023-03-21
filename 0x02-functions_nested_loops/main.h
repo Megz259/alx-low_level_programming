fe5dfa30fc04744a038b410bf8db3e8262368076
Megz259 <megan.hodsonrsa@gmail.com>
2023-03-21 13:05:14 -0700

    update

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,7 +12,9 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void pringt_to_98(int n);
+void print_to_98(int n);
 void print_times_table(int n);
+void print_times_table(int n);
+
 
 #endif
