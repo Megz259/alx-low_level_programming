fda6255125b50aa9e73345c18ff1ff83676f862e
Megz259 <megan.hodsonrsa@gmail.com>
2023-05-03 00:48:49 -0700

    Print list task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/*
+ * print_listint - prints all the elements of a list
+ * listint - singly linked list
+ * @h: singly linked list to print
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	printf("/n[h] =>");
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
