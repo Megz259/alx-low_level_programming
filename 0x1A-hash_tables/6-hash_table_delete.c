a3cbd6b6e5dbb392787134d6805bfd5ea32ccc81
Megz259 <megan.hodsonrsa@gmail.com>
2024-04-12 09:30:30 -0700

    Task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - free table 
+ * @ht: pointer to table
+ *
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int j = 0;
+	hash_node_t *buck;
+	hash_node_t *buck_free;
+
+	if (!ht)
+		return;
+
+	for (j = 0; j <ht->size; j++)
+	{
+		buck = ht->array[j];
+		while (buck)
+		{
+			buck_free = buck;
+			buck = buck->next;
+			if (buck_free->key)
+				free(buck_free->key);
+			if (buck_free->value)
+				free(buck_free->value);
+			free(buck_free);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
